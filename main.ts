eac62b78e670d80331961dedd1f25c3134361da5
jackbassman <79383566+jackbassman@users.noreply.github.com>
2021-03-25 02:46:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,32 +27,19 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.clearScreen()
     }
     if (receivedNumber == 4) {
-        if (true) {
-            basic.showString("LOUD")
-            music.setBuiltInSpeakerEnabled(true)
-            music.setVolume(255)
-            for (let index = 0; index < 1; index++) {
-                music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
-            }
-            basic.pause(2000)
+        basic.showString("LOUD")
+        music.setBuiltInSpeakerEnabled(true)
+        music.setVolume(255)
+        for (let index = 0; index < 3; index++) {
+            music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
         }
-    }
-    if (input.logoIsPressed()) {
-        if (receivedNumber == 5) {
-            basic.showString("CLEAR")
-            for (let index = 0; index < 5; index++) {
-                soundExpression.yawn.play()
-            }
-            music.stopAllSounds()
-        }
+        basic.pause(2000)
+        basic.clearScreen()
     }
 })
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
-    music.setBuiltInSpeakerEnabled(true)
-    soundExpression.slide.play()
     basic.showString("HELP")
-    soundExpression.slide.play()
     basic.pause(5000)
     basic.clearScreen()
 })
@@ -63,36 +50,34 @@ input.onGesture(Gesture.LogoUp, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(2)
+    radio.sendNumber(5)
     basic.showString("LEFT")
     basic.pause(2000)
     basic.clearScreen()
 })
 input.onSound(DetectedSound.Loud, function () {
-    input.setSoundThreshold(SoundThreshold.Loud, 228)
-    basic.showString("LOUD")
     radio.sendNumber(4)
-    music.setBuiltInSpeakerEnabled(true)
-    music.setVolume(255)
-    music.ringTone(523)
-    basic.pause(2000)
-    music.stopAllSounds()
+    input.setSoundThreshold(SoundThreshold.Loud, 204)
+    led.enable(true)
+    led.setBrightness(128)
+    led.plotBarGraph(
+    input.soundLevel(),
+    255
+    )
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
-    music.setBuiltInSpeakerEnabled(true)
-    soundExpression.happy.play()
     basic.showString("DINNER")
     basic.pause(5000)
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     if (true) {
+        music.setBuiltInSpeakerEnabled(false)
+        input.setAccelerometerRange(AcceleratorRange.TwoG)
         radio.sendNumber(3)
-        music.setBuiltInSpeakerEnabled(true)
-        input.setAccelerometerRange(AcceleratorRange.FourG)
-        music.playMelody("C5 A B G A F G E ", 120)
         basic.showNumber(input.acceleration(Dimension.Strength))
+        music.playMelody("C5 A B G A F G E ", 120)
         basic.pause(2000)
     }
 })
@@ -102,11 +87,9 @@ input.onGesture(Gesture.TiltRight, function () {
     basic.pause(2000)
     basic.clearScreen()
 })
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    radio.sendNumber(5)
-    soundExpression.twinkle.play()
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    soundExpression.yawn.play()
     music.stopAllSounds()
-    music.setBuiltInSpeakerEnabled(false)
 })
 radio.setGroup(1)
 radio.setTransmitPower(7)
